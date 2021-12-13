14fb8f3e4d9abae454e7e98731c7c6a104867d42
Fernadno Aldair <ferpool_ald@hotmail.com>
2021-12-13 13:33:54 -0600

    send udp

--- Andon-TerminalG/Form1.Designer.cs
+++ Andon-TerminalG/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.recivetext = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -61,11 +62,20 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
             // 
+            // recivetext
+            // 
+            this.recivetext.Location = new System.Drawing.Point(414, 162);
+            this.recivetext.Multiline = true;
+            this.recivetext.Name = "recivetext";
+            this.recivetext.Size = new System.Drawing.Size(163, 147);
+            this.recivetext.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.recivetext);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -82,6 +92,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox recivetext;
     }
 }
 
--- Andon-TerminalG/Form1.cs
+++ Andon-TerminalG/Form1.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// System
+using System.Net;
+using System.Net.Sockets;
+
+
+
 namespace Andon_TerminalG
 {
     public partial class Form1 : Form
@@ -24,7 +30,24 @@ namespace Andon_TerminalG
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "hello";
+            textBox1.Text = textBox1.Text + "hello";
+
+                UdpClient udpClient = new UdpClient();
+            
+            try {
+                udpClient.Connect(hostname: "127.0.0.1" ,port:4023);
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(textBox1.Text.ToString());
+                udpClient.Send(sendBytes, sendBytes.Length);
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                recivetext.Text = returnData.ToString();
+
+            }
+            catch ( Exception fault ) {
+                textBox1.Text = fault.ToString();
+                    }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
